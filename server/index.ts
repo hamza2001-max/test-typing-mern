93ec132bf5e2ba683135ba130d0a71ef9ffe8873
Hamza Ali <ellawliet1007@gmail.com>
2023-08-18 04:36:16 -0700

    final push

--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,15 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({path: __dirname+'/.env'});
-}
-// require("dotenv").config();
-const path = require('path');
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const wpmRoute = require("./routes/wpmRoutes");
 const userRoute = require("./routes/userRoutes");
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/wpm/", wpmRoute);
 app.use("/api/user/", userRoute);
-
-app.use('/some-route', require(path.join(__dirname, 'api', 'routes', 'route.js')));
-
-// static files (build of your frontend)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client', 'build')));
-  app.get('/*', (req:any, res:any) => {
-    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
-  })
-}
 
 mongoose
   .connect(process.env.CONNECTION)
